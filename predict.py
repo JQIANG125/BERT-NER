760e518c3551e31680f4a2bc4b711546f7a1349a
JQIANG125 <32261300+JQIANG125@users.noreply.github.com>
2020-02-10 01:37:29 +0800

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,21 @@
+from bert import Ner
+
+model = Ner("/content/out")
+textlist=[]
+labellist=[]
+with open ('/content/data.txt','a+') as file:
+	for i in file:
+		textlist.append(i[0:-1])
+
+for i in textlist:
+	labellist.append(model.predict(i))
+with open('/content/label.txt','a+') as label_file
+	for i in labellist:
+		a=''
+		for j in i:
+			a=a+j['tag']
+		a=a+'\n'
+		label_file.write(a)
+
+
+
